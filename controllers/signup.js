ad4a7ba6906b4f09151c803e5b0f58b5ab60fa05
Pavel Chlebek <pavelchlebek990@gmail.com>
2021-03-23 08:17:15 +0100

    signup new users to craigs db

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,14 +5,14 @@ exports.userSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   mongoQuery(async (client) => {
-    const usersColl = client.db("movies").collection("users");
+    const usersColl = client.db("craigs").collection("users");
     const userExists = await usersColl.findOne({ email: email });
     if (userExists) {
       const message = `Email: ${email} již existuje! Zkuste se zaregistrovat znovu s jinou emailovou adresou!`;
       res.status(202).json({ message: message });
     } else {
       mongoQuery((client) => {
-        const usersColl = client.db("movies").collection("users");
+        const usersColl = client.db("craigs").collection("users");
         usersColl.insertOne({ name: name, email: email, password: password, movies: [] });
       });
       const message = `Uživatel: ${name} s emailovou adresou: ${email} byl vytvořen!`;
